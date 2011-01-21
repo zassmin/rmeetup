e97d69686ddf2411fc09a2086a9fc9984b3cb0ae
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-21 01:01:50 -0500

    Generalized City model

--- lib/rmeetup/type/city.rb
+++ lib/rmeetup/type/city.rb
@@ -7,17 +7,32 @@ module RMeetup
     # Used to access result attributes as well
     # as progammatically fetch relative data types
     # based on this city.
+    
+    # Edited by Jason Berlinsky on 1/20/11 to allow for arbitrary data access
+    # See http://www.meetup.com/meetup_api/docs/cities/ for available fields
+    
     class City
-      attr_accessor :city, :lat, :lon, :country, :state, :zip, :members
+      
+      attr_accessor :city
       
       def initialize(city = {})
-        self.city         = city['city']
-        self.lat          = city['lat'].to_f
-        self.lon          = city['lon'].to_f
-        self.country      = city['country']
-        self.state        = city['state']
-        self.zip          = city['zip']
-        self.members      = city['members'].to_i
+        self.city = city
+      end
+      
+      def method_missing(id, *args)
+        return city[id.id2name].toutf8
+      end
+      
+      # Special accessors that need typecasting or other parsing
+      
+      def lat
+        return city['lat'].to_f
+      end
+      def lon
+        return city['lon'].to_f
+      end
+      def members
+        return city['members'].to_i
       end
     end
   end
