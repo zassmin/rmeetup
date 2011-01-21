22c7333f220ba211d0d7ebadeb9e49bed849ebdc
Jason Berlinsky <jason@jasonberlinsky.com>
2011-01-21 01:16:47 -0500

    Generalized Topic model

--- lib/rmeetup/type/topic.rb
+++ lib/rmeetup/type/topic.rb
@@ -7,17 +7,31 @@ module RMeetup
     # Used to access result attributes as well
     # as progammatically fetch relative data types
     # based on this topic.
+    
+    # Edited by Jason Berlinsky on 1/20/11 to allow for arbitrary data access
+    # See http://www.meetup.com/meetup_api/docs/topics/ for available fields
+    
     class Topic
-      attr_accessor :name, :urlkey, :id, :description, :members, :link, :updated
+      attr_accessor :topic
       
       def initialize(topic = {})
-        self.name         = topic['name']
-        self.urlkey       = topic['urlkey']
-        self.id           = topic['id'].to_i
-        self.description  = topic['description']
-        self.members      = topic['members'].to_i
-        self.link         = topic['link']
-        self.updated      = DateTime.parse(topic['updated'])
+        self.topic = topic
+      end
+      
+      def method_missing(id, *args)
+        return self.topic[id.id2name].toutf8
+      end
+      
+      # Special accessors that need typecasting or other parsing
+      
+      def id
+        return self.topic['id'].to_i
+      end
+      def members
+        return self.topic['members'].to_i
+      end
+      def updated
+        return DateTime.parse(self.topic['updated'])
       end
     end
   end
